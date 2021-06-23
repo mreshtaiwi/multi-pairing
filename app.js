77f46dda83e8165288b353a2bfbd46b83c4dd8d8
mreshtaiwi <mr.eshtaiwi@gmail.com>
2021-06-23 12:28:32 +0300

    pair programming from shihab as driver

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 'use strict';
 let num1 = 5;
 let num2 = 6;
-let multi = num1 * num2;
+// let multi = num1 * num2;
 
-console.log(multi);
+// console.log(multi);
+
+let multi = function (x, y) {
+    console.log(x * y);
+}
+multi(num1, num2);
